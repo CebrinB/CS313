3752fce8fcfd79e69ca5a6c2d57785309a6cad32
CebrinB <cebrin@byui.edu>
2021-01-16 22:12:27 -0600

    homepage

--- web/index.php
+++ web/index.php
@@ -11,7 +11,7 @@
     <link href="copy from Google Fonts" rel="stylesheet">
   </head>
   <body>
-  <nav class="navbar">
+  <nav class="navbar navbar-dark">
   <div class="container-fluid">
     <div class="navbar-header">
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
@@ -19,19 +19,19 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>                        
       </button>
-      <a class="navbar-brand" href="#">Logo</a>
+      <a class="navbar-brand" href="#">Cebrin</a>
     </div>
     <div class="collapse navbar-collapse" id="myNavbar">
       <ul class="nav navbar-nav">
         <li class="active"><a href="#">Home</a></li>
         <li><a href="#">About</a></li>
         <li class="dropdown">
-        <a class="dropdown-toggle" data-toggle="dropdown" href="#">Projects
+        <a class="dropdown-toggle" data-toggle="dropdown" href="#">Assignments
         <span class="caret"></span></a>
         <ul class="dropdown-menu">
-          <li><a href="#">Page 1-1</a></li>
-          <li><a href="#">Page 1-2</a></li>
-          <li><a href="#">Page 1-3</a></li>
+          <li><a href="#">Coming Soon</a></li>
+          <li><a href="#"></a></li>
+          <li><a href="#"></a></li>
         </ul>
       </li>
         <li><a href="#">Contact</a></li>
@@ -53,18 +53,18 @@
     <!-- Wrapper for slides -->
     <div class="carousel-inner" role="listbox">
       <div class="item active">
-        <img src="https://placehold.it/1200x400?text=IMAGE" alt="Image">
+        <img src="summer-783347_1920.jpg" alt="Summer garden with flowers">
         <div class="carousel-caption">
-          <h3>Sell $</h3>
-          <p>Money Money.</p>
+          <h3>I love to</h3>
+          <p>garden</p>
         </div>      
       </div>
 
       <div class="item">
-        <img src="https://placehold.it/1200x400?text=Another Image Maybe" alt="Image">
+        <img src="cookies-1835414_1920.jpg" alt="Blueberry cookies with flowers on the side">
         <div class="carousel-caption">
-          <h3>More Sell $</h3>
-          <p>Lorem ipsum...</p>
+          <h3>I love to </h3>
+          <p>bake treats</p>
         </div>      
       </div>
     </div>
@@ -81,29 +81,32 @@
 </div>
   
 <div class="container text-center">    
-  <h3>What We Do</h3><br>
+  <h3>What I Do</h3><br>
   <div class="row">
     <div class="col-sm-4">
-      <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
-      <p>Current Project</p>
+      <img src="mom-863055_1920.jpg" class="img-responsive" style="width:100%" alt="Image">
+      <p>Find Joy Raising my Children by Day</p>
     </div>
     <div class="col-sm-4"> 
-      <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
-      <p>Project 2</p>    
+      <img src="monkey-1197100_1920.jpg" class="img-responsive" style="width:100%" alt="Image">
+      <p>Code Monkey by Night</p>    
     </div>
     <div class="col-sm-4">
       <div class="well">
-       <p>Some text..</p>
+       <p>See previous assignments at my <a href="https://cebrinb.github.io/">GitHub</a></p>
       </div>
       <div class="well">
-       <p>Some text..</p>
+       <p>Check me out on <a href="https://www.linkedin.com/in/cebrinbillings/">LinkedIn</a></p>
       </div>
     </div>
   </div>
 </div><br>
 
 <footer class="container-fluid text-center">
-  <p>Footer Text</p>
+  <p>Garden image by <a href="https://pixabay.com/users/jillwellington-334088/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=783347">Jill Wellington</a> from <a href="https://pixabay.com/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=783347">Pixabay</a></p>
+  <p>Cookie image by <a href="https://pixabay.com/users/pexels-2286921/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=1835414">Pexels</a> from <a href="https://pixabay.com/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=1835414">Pixabay</a></p>
+  <p>Mother image by <a href="https://pixabay.com/photos/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=863055">Free-Photos</a> from <a href="https://pixabay.com/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=863055">Pixabay</a></p>
+  <p>Monkey image by <a href="https://pixabay.com/users/joelfotos-767874/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=1197100">Joel santana Joelfotos</a> from <a href="https://pixabay.com/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=1197100">Pixabay</a></p>
 </footer>
     <script src="main.js" type="module"></script>
   </body>
