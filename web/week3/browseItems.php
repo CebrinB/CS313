020e10a1a802d1679858eccb7427ae579929aa4e
CebrinB <cebrin@byui.edu>
2021-01-18 13:48:12 -0600

    week3

---
+++ web/week3/browseItems.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+  <?php
+
+  include '../head.php';
+
+  ?>
+  <body>
+    <?php
+
+      include '../navbar.php';
+
+    ?>
+    <div>Howdy</div>
+    <?php
+
+      include 'footer.php';
+
+    ?>
+    <script src="main.js" type="module"></script>
+  </body>
+</html>
