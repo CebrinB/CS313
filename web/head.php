4575add70e206e37967f8099d9c00909634b3963
CebrinB <cebrin@byui.edu>
2021-01-27 14:29:57 -0600

    week3

--- web/head.php
+++ web/head.php
@@ -9,6 +9,7 @@
   <script>
       console.log("here2");
       function addToCart() {
+        debugger;
         var xmlhttp = new XMLHttpRequest();
         xmlhttp.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {
