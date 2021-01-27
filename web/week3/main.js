648693c0984200be91e00ef03a056529ad67a80d
CebrinB <cebrin@byui.edu>
2021-01-27 16:35:45 -0600

    week3

--- web/week3/main.js
+++ web/week3/main.js
@@ -1,19 +1,62 @@
 function renderShoppingCartItems(parent) {
-  let div = document.createElement('div');
-  div.setAttribute('id', 'instructions');
-  div.innerHTML =`
-  <div>
-    <p>Add a horse to see it's possible coat color!</p>
-  </div>`;
-  parent.appendChild(div);
-  return;
-}
+  let numSaddles = document.querySelector('#saddles').innerHTML;
+  let numBridles = document.querySelector('#bridles').innerHTML;
+  let numBlankets = document.querySelector('#blankets').innerHTML;
+
+  let i = 0;
+  while (i < numSaddles) {
+    let item = document.createElement('li');
+        
+        /*item.addEventListener('click', e => {
+          let panel = e.target.nextElementSibling;
+          if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+          } else {
+          panel.style.maxHeight = "300px";
+          }
+        });*/
+
+        let pic = document.createElement('a');
+        pic.innerHTML = `
+        <img class="pic" src="saddle.jfif" alt="picture of dressage saddle">`;
+        /*pic.addEventListener('click', e => {
+          let panel = e.target.parentElement.parentElement.nextElementSibling;
+          if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+          } else {
+          panel.style.maxHeight = "300px";
+          }
+        });*/
+  
+        let name = document.createElement('span');
+        name.innerHTML = 'Black Dressage Saddle';
+        /*name.addEventListener('click', e => {
+          let panel = e.target.parentElement.nextElementSibling;
+          if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+          } else {
+          panel.style.maxHeight = "300px";
+          }
+        });*/
+        
+
+        let del = document.createElement('a');
+        del.innerHTML = `
+        <i class="fas fa-plus-circle cancel"</i>`;
+        /*del.addEventListener('click', this.removeHorse.bind(this, horse));*/
+  
+        item.appendChild(pic);
+        item.appendChild(name);
+        item.appendChild(del);
+  
+        parent.appendChild(item);
+    }
+  }
 
 
 
 
 
-console.log(document.querySelector('#saddles'));
 
 
 window.addEventListener("load", () => {
