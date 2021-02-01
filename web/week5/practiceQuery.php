f56368b9598bbeb063f7f6dc33b4a0cca2e165ce
CebrinB <cebrin@byui.edu>
2021-02-01 16:46:48 -0600

    week5

--- web/week5/practiceQuery.php
+++ web/week5/practiceQuery.php
@@ -24,11 +24,6 @@ catch (PDOException $ex)
   die();
 }
 
-foreach ($db->query('SELECT username, password FROM note_user') as $row)
-{
-  echo 'user: ' . $row['username'];
-  echo ' password: ' . $row['password'];
-  echo '<br/>';
-}
+echo 'This worked.';
 
 ?>
