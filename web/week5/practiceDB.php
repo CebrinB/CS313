e5af934faeb03742bb80de539cae75c9b588cf3b
CebrinB <cebrin@byui.edu>
2021-02-06 16:11:05 -0600

    week5

--- web/week5/practiceDB.php
+++ web/week5/practiceDB.php
@@ -1,43 +1,52 @@
 <?php
-// Start the session
-session_start();
+  // Start the session
+  session_start();
+  
+  try
+  {
+    $dbUrl = getenv('DATABASE_URL');
 
-try
-{
-  $dbUrl = getenv('DATABASE_URL');
+    $dbOpts = parse_url($dbUrl);
 
-  $dbOpts = parse_url($dbUrl);
+    $dbHost = $dbOpts["host"];
+    $dbPort = $dbOpts["port"];
+    $dbUser = $dbOpts["user"];
+    $dbPassword = $dbOpts["pass"];
+    $dbName = ltrim($dbOpts["path"],'/');
 
-  $dbHost = $dbOpts["host"];
-  $dbPort = $dbOpts["port"];
-  $dbUser = $dbOpts["user"];
-  $dbPassword = $dbOpts["pass"];
-  $dbName = ltrim($dbOpts["path"],'/');
+    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
-  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $ex)
-{
-  echo 'Error!: ' . $ex->getMessage();
-  die();
-}
-
-//if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-echo "<h1>Scripture Resources</h1></br>";
-  foreach ($db->query('SELECT item_id, item_name, item_price, item_description FROM ecommerce.item') as $row)
-    {
-      echo "<div><a href=\"/scriptureDetails.php\"><b>" . $row['item_id'] . " " . $row['item_name'] . ":" . $row['item_price'] . "</b></a></div>";
-    }
-//}
+  }
+  catch (PDOException $ex)
+  {
+    echo 'Error!: ' . $ex->getMessage();
+    die();
+  }
+  
+  
+  include 'head.php';
 
 ?>
 
-<html>
-  <form method="POST">
-    <input type="text" name="book">
-    <input type="submit" value="submit">
-  </form>
-</html>
+  <body>
+    <?php include 'navbar.php'; ?>
 
+    <!-- Page Content -->
+    <div>
+    <?php 
+      echo "<h1>Items</h1></br>";
+      foreach ($db->query('SELECT item_id, item_name, item_price, item_description FROM ecommerce.item') as $row)
+        {
+          echo "<div><a href=\"/scriptureDetails.php\"><b>" . $row['item_id'] . " " . $row['item_name'] . ":" . $row['item_price'] . "</b></a></div>";
+        }
+        
+    ?></div>
+
+
+ 
+
+    <?php include 'footer.php'; ?>
+  </body>
+</html>
