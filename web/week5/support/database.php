8630e6c6a8f29486e2eab02d7432b97fdf1ca703
CebrinB <cebrin@byui.edu>
2021-02-06 15:37:14 -0600

    week5

--- web/week5/support/database.php
+++ web/week5/support/database.php
@@ -16,6 +16,10 @@ try
   $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+  $statement = $db->prepare('SELECT item_id, item_name, item_price, item_description FROM ecommerce.item');
+  $statement->execute();
+
 }
 catch (PDOException $ex)
 {
@@ -23,19 +27,6 @@ catch (PDOException $ex)
   die();
 }
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-echo "<h1>Scripture Resources</h1></br>";
-  foreach ($db->query('SELECT item_id, item_name, item_price, item_description FROM ecommerce.item') as $row)
-    {
-      echo "<div><a href=\"/scriptureDetails.php\"><b>" . $row['item_id'] . " " . $row['item_name'] . ":" . $row['item_price'] . "</b></a></div>";
-    }
-}
+return $db;
 
 ?>
-
-<html>
-  <form method="POST">
-    <input type="text" name="book">
-    <input type="submit" value="submit">
-  </form>
-</html>
