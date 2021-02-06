0da3055310fe5cada872289c0e47fb848a7f66c9
CebrinB <cebrin@byui.edu>
2021-02-06 17:57:54 -0600

    week5

--- web/week5/view/shop.php
+++ web/week5/view/shop.php
@@ -4,8 +4,8 @@
   
   include '../support/database.php';
   
-  if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    print_r($_POST);
+  if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    print_r($_GET);
   }
   echo 'test';
 
