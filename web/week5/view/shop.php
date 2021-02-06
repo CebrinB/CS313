3add75185b2046163d2caba85596a81247d50738
CebrinB <cebrin@byui.edu>
2021-02-06 16:16:17 -0600

    week5

--- web/week5/view/shop.php
+++ web/week5/view/shop.php
@@ -2,34 +2,7 @@
   // Start the session
   session_start();
   
-  try
-  {
-    $dbUrl = getenv('DATABASE_URL');
-
-    $dbOpts = parse_url($dbUrl);
-
-    $dbHost = $dbOpts["host"];
-    $dbPort = $dbOpts["port"];
-    $dbUser = $dbOpts["user"];
-    $dbPassword = $dbOpts["pass"];
-    $dbName = ltrim($dbOpts["path"],'/');
-
-    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-  }
-  catch (PDOException $ex)
-  {
-    echo 'Error!: ' . $ex->getMessage();
-    die();
-  }
-  
-  echo "<h1>Items</h1></br>";
-      foreach ($db->query('SELECT item_id, item_name, item_price, item_description FROM ecommerce.item') as $row)
-        {
-          echo "<div>" . $row['item_id'] . " " . $row['item_name'] . ":" . $row['item_price'] . "</div>";
-        }
+  include '../support/database.php';
   
   
   include 'head.php';
