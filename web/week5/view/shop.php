260c6bf630937f1148f28aa04996aef2c16c8f57
CebrinB <cebrin@byui.edu>
2021-02-06 15:38:21 -0600

    week5

--- web/week5/view/shop.php
+++ web/week5/view/shop.php
@@ -5,6 +5,12 @@
   
   include 'head.php';
 
+  echo "<h1>Scripture Resources</h1></br>";
+  foreach ($db->query('SELECT item_id, item_name, item_price, item_description FROM ecommerce.item') as $row)
+    {
+      echo "<div><a href=\"/scriptureDetails.php\"><b>" . $row['item_id'] . " " . $row['item_name'] . ":" . $row['item_price'] . "</b></a></div>";
+    }
+
 ?>
 
   <body>
