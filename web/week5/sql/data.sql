4e92bd18d4960cff3e4857520f91531a74785ddd
CebrinB <cebrin@byui.edu>
2021-02-06 16:54:14 -0600

    week5

---
+++ web/week5/sql/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO ecommerce.item (item_name, item_price, item_description) VALUES
+  ('Bridle', 299.95, 'Brown leather dressage bridle with double bit'),
+  ('Blanket', 199.95, 'Blue quilted winter horse blanket');
+
+  
