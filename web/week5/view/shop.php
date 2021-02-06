e4ce21a91a3f1355a9cdf09888c65cdf2ffc2df9
CebrinB <cebrin@byui.edu>
2021-02-06 17:00:28 -0600

    week5

--- web/week5/view/shop.php
+++ web/week5/view/shop.php
@@ -20,10 +20,13 @@
     $c = 1;
 
       echo "<h1>Items</h1></br>";
+      
+      
       foreach ($db->query('SELECT item_id, item_name, item_price, item_description FROM ecommerce.item') as $row)
         {
+          if ($r < 3) {
           echo
-          '<div class="card" style="width: 18rem;">
+          '<div class="card col-sm-4">
             <img class="card-img-top" src="../images/' . $row['item_name'] . $row['item_id'] . '.jpg" width="100%" alt="">
             <div class="card-body">  
               <h5 class="card-title">'. $row['item_name'] .'<span class="text-right">$'. $row['item_price'] .'</span></h5>
@@ -31,7 +34,9 @@
               <a href="#" class="btn btn-primary">Add to Cart</a>
             </div>
           </div>';
-        
+          $r++;
+          }
+          
         }
         
     ?></div>
