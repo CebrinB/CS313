0097be325c5aa5b5c0d1724ffc8fe52ba9927b4f
CebrinB <cebrin@byui.edu>
2021-02-06 17:05:07 -0600

    week5

--- web/week5/view/home.php
+++ web/week5/view/home.php
@@ -24,7 +24,7 @@
       <div class="row">
         <div class="col-sm-4">
           <div class="card">
-            <img class="card-img-top" src="../images/saddle2.jpg" width="100%" alt="Black dressage saddle">
+            <img class="card-img-top" src="../images/Saddle4.jpg" width="100%" alt="Black dressage saddle">
             <div class="card-body">
               <h5 class="card-title">Saddles</h5>
               <p class="card-text">Shop our Dressage saddles</p>
@@ -33,12 +33,12 @@
           </div>
         </div>
         <div class="col-sm-4"> 
-          <img src="../images/bridle.jfif" class="img-responsive" style="width:100%" alt="Chestnut horse with bridle">
+          <img src="../images/Bridle5.jfif" class="img-responsive" style="width:100%" alt="Chestnut horse with bridle">
           <p>Bridles</p>
           <a href="shop.php" class="btn btn-primary">SHOP NOW</a>    
         </div>
         <div class="col-sm-4">
-          <img src="../images/blanket.jfif" class="img-responsive" style="width:100%" alt="Bay horse with blue blanket">
+          <img src="../images/Blanket6.jfif" class="img-responsive" style="width:100%" alt="Bay horse with blue blanket">
           <p>Blankets</p>
           <a href="shop.php" class="btn btn-primary">SHOP NOW</a>
         </div>
