c5ef531d9aecc15279bf5e2db26259778a52b2cd
CebrinB <cebrin@byui.edu>
2021-02-06 14:24:33 -0600

    week5

---
+++ web/week5/view/createReview.php
@@ -0,0 +1,17 @@
+<?php
+  // Start the session
+  session_start();
+  
+  include 'head.php';
+
+?>
+
+  <body>
+    <?php include 'navbar.php'; ?>
+
+    <!-- Page Content -->
+
+
+    <?php include 'footer.php'; ?>
+  </body>
+</html>
--- web/week5/view/navbar.php
+++ web/week5/view/navbar.php
@@ -7,24 +7,17 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>                        
       </button>
-      <a class="navbar-brand" href="#">Billings Saddlery</a>
+      <a class="navbar-brand" href="home.php"><h4>Billings Saddlery</h4></a>
     </div>
     <div class="collapse navbar-collapse" id="myNavbar">
       <ul class="nav navbar-nav">
-        <li class="active"><a href="/index.php">Home</a></li>
+        <li class="active"><a href="home.php">Home</a></li>
+        <li><a href="about.php">About</a></li>
         <li><a href="shop.php">Shop</a></li>
-        <li class="dropdown">
-        <a class="dropdown-toggle" data-toggle="dropdown" href="#">Assignments
-        <span class="caret"></span></a>
-        <ul class="dropdown-menu">
-          <li><a href="/week3/browseItems.php">week3 - Storefront</a></li>
-          <li><a href="/week5/practiceQuery.php">week5 - Practice Query</a></li>
-        </ul>
-      </li>
-        <li><a href="#">Contact</a></li>
+        <li><a href="contact.php">Contact</a></li>
       </ul>
       <ul class="nav navbar-nav navbar-right">
-        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+        <li><a href="cart.php"><span class="glyphicon glyphicon-shopping-cart"></span> Cart</a></li>
       </ul>
     </div>
   </div>
