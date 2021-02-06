f18f9f0759cd8828210edd265c1b0e8ded9bc56c
CebrinB <cebrin@byui.edu>
2021-02-06 17:26:22 -0600

    week5

--- web/week5/view/shop.php
+++ web/week5/view/shop.php
@@ -13,34 +13,41 @@
     <?php include 'navbar.php'; ?>
 
     <!-- Page Content -->
-    <div>
-    <?php 
-    
-    $r = 0;
-    $c = 1;
+    <div class="container-fluid">
+      <div class="row content">
+        <div class="col-sm-3 sidenav">
+          <h4>Categories</h4>
+          <ul class="nav nav-pills nav-stacked">
+            <li><a href="#">Saddles</a></li>
+            <li><a href="#">Bridles</a></li>
+            <li><a href="#">Blankets</a></li>
+          </ul><br>
+        </div>
+        <div class="col-sm-9">
+          <?php 
+            $r = 0;
+            $c = 1;
 
-      echo "<h1>Items</h1></br>";
-      
-      echo '<div class="row">';
-      foreach ($db->query('SELECT item_id, item_name, item_price, item_description FROM ecommerce.item') as $row)
-        {
-          if ($r < 3) {
-          echo
-          '<div class="card col-sm-4">
-            <img class="card-img-top" src="../images/' . $row['item_name'] . $row['item_id'] . '.jpg" width="100%" alt="">
-            <div class="card-body">  
-              <h5 class="card-title">'. $row['item_name'] .'<span class="text-right">$'. $row['item_price'] .'</span></h5>
-              <p class="card-text">' . $row['item_description'] .'</p>
-              <a href="#" class="btn btn-primary">Add to Cart</a>
-            </div>
-          </div>';
-          $r++;
-          }
-          
-        }
-        echo '</div>';
-        
-    ?></div>
+            echo "<h1>Items</h1></br>";
+            echo '<div class="row">';
+            foreach ($db->query('SELECT item_id, item_name, item_price, item_description FROM ecommerce.item') as $row)
+              {
+                echo
+                '<div class="card col-sm-4">
+                  <img class="card-img-top" src="../images/' . $row['item_name'] . $row['item_id'] . '.jpg" width="100%" alt="">
+                  <div class="card-body">  
+                    <h5 class="card-title">'. $row['item_name'] .'<span class="text-right">$'. $row['item_price'] .'</span></h5>
+                    <p class="card-text">' . $row['item_description'] .'</p>
+                    <a href="#" class="btn btn-primary">Add to Cart</a>
+                  </div>
+                </div>';
+              }
+            echo '</div>';  
+          ?>
+        </div>
+      </div>
+    </div>
+
 
 
  
