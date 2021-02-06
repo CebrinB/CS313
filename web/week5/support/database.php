96c0df86fea15947d72c9ec114437d792da4e80d
CebrinB <cebrin@byui.edu>
2021-02-06 15:59:48 -0600

    week5

--- web/week5/support/database.php
+++ web/week5/support/database.php
@@ -17,24 +17,12 @@ try
 
   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-  foreach ($db->query('SELECT item_id, item_name, item_price, item_description FROM ecommerce.item') as $row)
-    {
-      echo 'howdy';
-      echo "<div><a href=\"/scriptureDetails.php\"><b>" . $row['item_id'] . " " . $row['item_name'] . ":" . $row['item_price'] . "</b></a></div>";
-    }
 }
 catch (PDOException $ex)
 {
   echo 'Error!: ' . $ex->getMessage();
   die();
 }
-
-echo "<h1>Items</h1></br>";
-  foreach ($db->query('SELECT item_id, item_name, item_price, item_description FROM ecommerce.item') as $row)
-    {
-      echo 'howdy';
-      echo "<div><a href=\"/scriptureDetails.php\"><b>" . $row['item_id'] . " " . $row['item_name'] . ":" . $row['item_price'] . "</b></a></div>";
-    }
 
 return $db;
 
