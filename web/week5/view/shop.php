b43c0cbe9651b3060da1f80fe6fe2a5aeb6f4eb3
CebrinB <cebrin@byui.edu>
2021-02-06 19:19:54 -0600

    week5

--- web/week5/view/shop.php
+++ web/week5/view/shop.php
@@ -40,7 +40,7 @@
 
             echo "<h1>Items</h1></br>";
             echo '<div class="row">';
-            $query = 'SELECT * FROM ecommerce.item WHERE item_name = '".$filter.";
+            $query = 'SELECT * FROM ecommerce.item WHERE item_name = '".$filter."'';
             // $statement = $db->prepare($query);
             // $statement->execute();
             // $infos = $statement->fetchAll(PDO::FETCH_ASSOC);
