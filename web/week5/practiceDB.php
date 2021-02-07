cf80423999f644de2982521b678997ed3ab4fca8
CebrinB <cebrin@byui.edu>
2021-02-06 19:57:12 -0600

    week5

--- web/week5/practiceDB.php
+++ web/week5/practiceDB.php
@@ -16,21 +16,25 @@
     <div>
     <?php 
       echo "<h1>Items</h1></br>";
-      $query = 'SELECT * FROM ecommerce.item WHERE item_name :filt';
-            $statement = $db->prepare($query);
-            $stmt->bindValue(':filt', '%'.$_GET['filter'].'%', PDO::PARAM_STR);
-            $statement->execute();
-            $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
-            $statement->closeCursor();
+      $sql = 'SELECT * FROM team05.scriptures WHERE book LIKE :book';
+      $stmt = $db->prepare($sql);
+      $stmt->bindValue(':book', '%'.$_POST['book'].'%', PDO::PARAM_STR);
+      $stmt->execute();
+      $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+      $stmt->closeCursor();
 
-             foreach ($rows as $row) {
-               echo $row['item_id'];
-             }
+      foreach ($rows as $row){
+        echo "<div><a href=\"scripturesDetails.php?id=" . $row["id"] . "\"><b>" . $row['book'] . " " . $row['chapter'] . ":" . $row['verse'] . "</b></a></div>";
+      }
         
     ?></div>
 
-
- 
+<html>
+<form method="POST">
+<input type='text' name='book'>
+<input type='submit' value='submit'>
+</from>
+</html> 
 
     <?php include 'view/footer.php'; ?>
   </body>
