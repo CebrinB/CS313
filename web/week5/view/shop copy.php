5de6fe03f9c419886498d41cdd673522af0d3245
CebrinB <cebrin@byui.edu>
2021-02-12 22:02:42 -0600

    week6

--- web/week5/view/shop copy.php
+++ web/week5/view/shop copy.php
@@ -65,7 +65,7 @@
                       <h5 class="card-title">'. $row['item_name'] .'<span class="text-right">$'. $row['item_price'] .'</span></h5>
                       <p class="card-text">' . $row['item_description'] .'</p>
                       <a href="#" class="btn btn-primary">Add to Cart</a>
-                      <a href="#" class="btn btn-primary">Reviews</a>
+                      <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">Reviews</a>
                     </div>
                   </div>
                 </div>';
@@ -78,6 +78,27 @@
           ?>
           <div class="row"></div>
         </div>
+      </div>
+    </div>
+
+    <!-- Modal -->
+    <div id="myModal" class="modal fade" role="dialog">
+      <div class="modal-dialog">
+
+        <!-- Modal content-->
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal">&times;</button>
+            <h4 class="modal-title">Modal Header</h4>
+          </div>
+          <div class="modal-body">
+            <p>Some text in the modal.</p>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+          </div>
+        </div>
+
       </div>
     </div>
 
