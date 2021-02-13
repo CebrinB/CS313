266f16c3a471745d21357412680f2485fafb7349
CebrinB <cebrin@byui.edu>
2021-02-12 23:57:16 -0600

    week6

--- web/week5/model/main.js
+++ web/week5/model/main.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
   $('#myModal').on('shown.bs.modal', function () {
       $.ajax({
           type: 'GET',
-          url: "item_reviews.php", //this file has the calculator function code
+          url: "item_reviews.php?item_id=1", //this file has the calculator function code
           success:function(data){
            $('#showcal').html(data);
           }
--- web/week5/view/shop copy.php
+++ web/week5/view/shop copy.php
@@ -92,8 +92,8 @@
             <button type="button" class="close" data-dismiss="modal">&times;</button>
             <h4 class="modal-title">Modal Header</h4>
           </div>
-          <div class="modal-body">
-            <p>Some text in the modal.</p>
+          <div class="modal-body" id="#showCal">
+            
           </div>
           <div class="modal-footer">
             <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
