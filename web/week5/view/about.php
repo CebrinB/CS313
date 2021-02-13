fe39cd4cef9261f4b72692838deb1214b2e8c9df
CebrinB <cebrin@byui.edu>
2021-02-12 18:55:07 -0600

    week6

--- web/week5/view/about.php
+++ web/week5/view/about.php
@@ -10,6 +10,10 @@
     <?php include 'navbar.php'; ?>
 
     <!-- Page Content -->
+    <div>At Billings Saddlery we strive to create a streamlined online shopping experience that provides
+    affordability, visibility, and competitive pricing for our customers. Thank you for visiting 
+    our online store!
+    <a href="contact.php">Find a location</a></div>
 
 
     <?php include 'footer.php'; ?>
