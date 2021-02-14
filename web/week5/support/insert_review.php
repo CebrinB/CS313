84596b9b21b6b1be2fbb2d3ad4a7e1f68eed9f3e
CebrinB <cebrin@byui.edu>
2021-02-13 21:57:02 -0600

    week6

--- web/week5/support/insert_review.php
+++ web/week5/support/insert_review.php
@@ -2,18 +2,26 @@
 
 session_start();
 
+require_once 'database.php';
+
+
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   $user_name = htmlspecialchars($_POST['user_name']);
   $rating = htmlspecialchars($_POST['rating']);
   $title = htmlspecialchars($_POST['title']);
   $content = htmlspecialchars($_POST['content']);
   $item_id = htmlspecialchars($_POST['item_id']);
-
-
-
 }
 
-echo $rating;
+$sql = 'SELECT * FROM ecommerce.user WHERE user_name = '.$user_name;
+$stmt = $db->prepare($sql);
+$stmt->execute();
+$user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+$stmt->closeCursor();
+
+echo $user['user_id'];
 
 
 ?>
