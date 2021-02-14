0e6e7d8e1d4a9619aa3edf0620c8848daad8489e
CebrinB <cebrin@byui.edu>
2021-02-13 22:21:15 -0600

    week6

--- web/week5/support/insert_review.php
+++ web/week5/support/insert_review.php
@@ -11,9 +11,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   $rating = htmlspecialchars($_POST['rating']);
   $title = htmlspecialchars($_POST['title']);
   $content = htmlspecialchars($_POST['content']);
-  $item_id = intval(htmlspecialchars($_POST['item_id']));
+  $item_id = htmlspecialchars($_POST['item_id']);
 }
-echo $_POST['item_id'];
+
 echo $item_id;
 
 // $sql = 'SELECT * FROM ecommerce.user WHERE user_name = \''.$user_name.'\'';
