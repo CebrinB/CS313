66a082431100f82ce0a41113722b1e916d20ff4f
CebrinB <cebrin@byui.edu>
2021-02-13 23:10:30 -0600

    week6

--- web/week5/support/insert_review.php
+++ web/week5/support/insert_review.php
@@ -15,29 +15,29 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   $item_id = filter_input(INPUT_POST, 'item_id', FILTER_SANITIZE_NUMBER_INT);
 }
 
-echo $item_id;
 
-// $sql = 'SELECT * FROM ecommerce.user WHERE user_name = \''.$user_name.'\'';
-// $stmt = $db->prepare($sql);
-// $stmt->execute();
-// $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
-// $sql = "INSERT INTO ecommerce.review (user_id, rating, title, content, item_id) VALUES
-//         (:user_id, :rating, :title, :content, :item_id);";
-// $stmt = $db->prepare($sql);
-// $stmt->bindValue(':user_id', $user['user_id'], PDO::PARAM_INT);
-// $stmt->bindValue(':rating', $rating, PDO::PARAM_INT);
-// $stmt->bindValue(':title', $title, PDO::PARAM_STR);
-// $stmt->bindValue(':content', $content, PDO::PARAM_STR);
-// $stmt->bindValue(':item_id', $item_id, );
-// $stmt->execute();
+$sql = 'SELECT * FROM ecommerce.user WHERE user_name = \''.$user_name.'\'';
+$stmt = $db->prepare($sql);
+$stmt->execute();
+$user = $stmt->fetch(PDO::FETCH_ASSOC);
 
-// $stmt->closeCursor();
+$sql = "INSERT INTO ecommerce.review (user_id, rating, title, content, item_id) VALUES
+        (:user_id, :rating, :title, :content, :item_id);";
+$stmt = $db->prepare($sql);
+$stmt->bindValue(':user_id', $user['user_id'], PDO::PARAM_INT);
+$stmt->bindValue(':rating', $rating, PDO::PARAM_INT);
+$stmt->bindValue(':title', $title, PDO::PARAM_STR);
+$stmt->bindValue(':content', $content, PDO::PARAM_STR);
+$stmt->bindValue(':item_id', $item_id, PDO::PARAM_INT);
+$stmt->execute();
 
-// $new_page = "../view/item.php?item_id=$item_id";
+$stmt->closeCursor();
 
-// header("Location: $new_page");
-// die();
+$new_page = "../view/item.php?item_id=$item_id";
+
+header("Location: $new_page");
+die();
 
 
 ?>
