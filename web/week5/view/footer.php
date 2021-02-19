e6fdb6fda0fea26a732fee2c20e30709c5a84537
CebrinB <cebrin@byui.edu>
2021-02-19 12:42:34 -0600

    week07

--- web/week5/view/footer.php
+++ web/week5/view/footer.php
@@ -2,8 +2,14 @@
   <div>
     <div class="fluid-container">
       <div class="row">
-        <div class="col-md-4"><a class="brand white" href="home.php"><h3>Billings Saddlery</h3></a></div>
-        <div class="col-md-4">Example 2</div>
+        <div class="col-md-4"><a class="white" href="home.php"><h3>Billings Saddlery</h3></a></div>
+        <div class="col-md-4">
+          <div>© 2021 Billings Saddlery, Inc., All Rights Reserved.</br>
+              123 Sesame Street, North Pole, Antarctica 01234</br>
+              Not responsible for typographical errors. Prices subject to change without notice.</br>
+              This website was created for a school project and is not a real store.
+          </div>
+        </div>
         <div class="col-md-4">Example 3</div>
       </div>
     </div>
