ccb7375e7ee19c977c1a0c4490643858f4029c8d
CebrinB <cebrin@byui.edu>
2021-02-19 13:45:10 -0600

    week07

--- web/week5/view/footer.php
+++ web/week5/view/footer.php
@@ -1,23 +1,21 @@
-<footer class="container footer text-center white">
+<footer class="container-fluid footer text-center white">
   <div>
-    <div class="fluid-container">
-      <div class="row">
-        <div class="col-md-4"><a class="white" href="home.php"><h3>Billings Saddlery</h3></a></div>
-        <div class="col-md-4">
-          <div>© 2021 Billings Saddlery, Inc., All Rights Reserved.</br>
-              123 Sesame Street, North Pole, Antarctica 01234</br>
-              Not responsible for typographical errors. Prices subject to change without notice.</br></br>
-              This website was created for a school project and is not a real store.
-          </div>
+    <div class="row">
+      <div class="col-md-4"><a class="white" href="home.php"><h3>Billings Saddlery</h3></a></div>
+      <div class="col-md-4">
+        <div>© 2021 Billings Saddlery, Inc., All Rights Reserved.</br>
+            123 Sesame Street, North Pole, Antarctica 01234</br>
+            Not responsible for typographical errors. Prices subject to change without notice.</br></br>
+            This website was created for a school project and is not a real store.
         </div>
-        <div class="col-md-4">
-          <div><h3>Cebrin Billings</h3> <span class="glyphicon glyphicon-grain"></span> 
-                Software Engineering Student <span class="glyphicon glyphicon-grain"></span></br>
-                  Connect with me
-          </div>
-          <div>Fontawesome linkedin github</div>
-          <div>Email</div>
       </div>
+      <div class="col-md-4">
+        <div><h3>Cebrin Billings</h3> <span class="glyphicon glyphicon-grain"></span> 
+              Software Engineering Student <span class="glyphicon glyphicon-grain"></span></br>
+                Connect with me
+        </div>
+        <div>Fontawesome linkedin github</div>
+        <div>Email</div>
       </div>
     </div>
   </div>
