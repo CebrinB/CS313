f7ab3b279836465abd5ec3111c7a8a7f560dc3b4
CebrinB <cebrin@byui.edu>
2021-02-19 17:27:08 -0600

    week07

--- web/week5/view/contact.php
+++ web/week5/view/contact.php
@@ -19,7 +19,7 @@
           <h4>Locations</h4>
           
             <ul class="nav nav-pills nav-stacked">
-              <?php 
+               
               <li><a href="shop.php" type="button">> All</a></li>
               <li><a type="button">> <input type="submit" name="filter" value="Saddles"></a></li>
               <li><a type="button">> <input type="submit" name="filter" value="Bridles"></a></li>
