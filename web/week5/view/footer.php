1dacec7953a73327e185d8dd3c92a6f2c068ec28
CebrinB <cebrin@byui.edu>
2021-02-19 12:27:03 -0600

    week07

--- web/week5/view/footer.php
+++ web/week5/view/footer.php
@@ -1,3 +1,12 @@
 <footer class="container-fluid text-center white">
+  <div>
+    <div class="fluid-container">
+      <div class="row">
+        <div class="col-md-4">Example</div>
+        <div class="col-md-4">Example 2</div>
+        <div class="col-md-4">Example 3</div>
+      </div>
+    </div>
+  </div>
   <p>Website by Cebrin Billings, all rights reserved. 2021</p>
 </footer>
