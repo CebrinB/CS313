32cfea1c7902b533a8870c77c620e5480c506ad7
CebrinB <cebrin@byui.edu>
2021-02-19 14:06:14 -0600

    week07

--- web/week5/view/about.php
+++ web/week5/view/about.php
@@ -13,10 +13,10 @@
     <div class="container text-center about">
       <div class="col-sm-2"></div>
       <div class="col-lg-8">
-        <div>At Billings Saddlery we strive to create a streamlined online shopping experience that provides
-            affordability, visibility, and competitive pricing for our customers. 
+        <div><h4>At Billings Saddlery we strive to create a streamlined online shopping experience that provides
+            affordability, visibility, and competitive pricing for our customers.</h4> 
         </div>
-        <div>Thank you for visiting our online store!</br></br></div>
+        <div><h4>Thank you for visiting our online store!</h4></br></br></div>
         <div><a class="homeshop" href="contact.php">Find a location</a></div>
       </div>
       <div class="col-sm-2"></div>
