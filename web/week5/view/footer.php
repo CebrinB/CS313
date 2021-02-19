2f93adddc6fee85ed966de250529efcac6e73c0c
CebrinB <cebrin@byui.edu>
2021-02-19 12:33:32 -0600

    week07

--- web/week5/view/footer.php
+++ web/week5/view/footer.php
@@ -2,7 +2,7 @@
   <div>
     <div class="fluid-container">
       <div class="row">
-        <div class="col-md-4">Example</div>
+        <div class="col-md-4"><a class="brand white" href="home.php"><h3>Billings Saddlery</h3></a></div>
         <div class="col-md-4">Example 2</div>
         <div class="col-md-4">Example 3</div>
       </div>
