fb5b230df93786b4ba32c7116a94e77f746dfa24
CebrinB <cebrin@byui.edu>
2021-02-20 15:30:05 -0600

    week07

--- web/week5/view/signUp.php
+++ web/week5/view/signUp.php
@@ -1,36 +1,35 @@
-<?php
-
-session_start();
-
-
-include 'head.php';
-
-?>
-<body>
-<?php include 'navbar.php'; ?>
-<h1>Sign Up</h1>
-<?php
-if (isset($_SESSION['message'])) {
-        echo $_SESSION['message'];
-       }
-?>
-<div id="error"></div>
-<form onsubmit="return clientSideStretch3()" method="post" action="login.php">
-<label>Username:</label><br>
-<input name="username" type=text><br>
-<label>Password:</label><br>
-<?php if (isset($mark)) {
-        echo "<span class='notice'>*</span>";
-       }?><input id="password1" name="password" type="password" pattern="^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{7,}$"><br>
-<label>Re-enter Password:</label><br>
-<?php if (isset($mark)) {
-        echo "<span class='notice'>*</span>";
-       }?><input id="password2" name="password2" pattern="^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{7,}$" type="password"><br>
-<button type="submit">Sign Up</button>
-<input type="hidden" name="action" value="signUp">
-</form>
-<?php include 'footer.php'; ?>
-<script src="main.js"></script>
+<?php include 'head.php'; ?>
+<body class="text-center">
+  <?php include 'navbar.php'; ?>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-4"></div>
+      <div class="col-lg-4 well">
+        <h1>Create an Account</h1>
+        <?php if (isset($_SESSION['message'])) {
+          echo $_SESSION['message'];
+        } ?>
+        <form action="login.php" method="POST">
+          <label for="username" class="sr-only">Username:</label><br>
+          <input name="username" id="username" type="text" placeholder="Username" required autofocus>
+          <label for="password" class="sr-only">Password:</label><br>
+          <?php if (isset($mark)) {
+            echo '<span class="notice">*</span>';
+          } ?>
+          <input name="password" id="password" type="password" pattern="^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{7,}$" required>
+          <label for="checkpassword" class="sr-only">Confirm password:</label><br>
+          <?php if (isset($mark)) {
+            echo '<span class="notice">*</span>';
+          } ?>
+          <input name="password2" id="checkpassword" type="password" pattern="^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{7,}$" required>
+          <button type="submit">Sign up</button>
+          <input type="hidden" name="action" value="signUp">
+        </form>
+        <?php include 'footer.php'; ?>     
+      </div>
+      <div class="col-lg-4"></div>
+    </div> <!-- close row -->
+  </div> <!-- close container -->
+  <?php include 'footer.php'; ?>
 </body>
 </html>
-<?php unset($_SESSION['message'])?>
