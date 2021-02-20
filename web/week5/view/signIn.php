d3462702cc7ccfa47c66e32af05e836cbcab9b8d
CebrinB <cebrin@byui.edu>
2021-02-20 14:44:24 -0600

    week07

--- web/week5/view/signIn.php
+++ web/week5/view/signIn.php
@@ -4,7 +4,7 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-lg-3"></div>
-                <div class="col-lg-6">
+                <div class="col-lg-6 well">
                     <h1>Sign In</h1>
                     <?php
                     if (isset($_SESSION['message'])) {
