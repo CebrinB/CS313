bc414db3a68891a051eb96a8b0a5550cbb83718e
CebrinB <cebrin@byui.edu>
2021-02-19 22:32:19 -0600

    week07

--- web/week5/view/item.php
+++ web/week5/view/item.php
@@ -51,29 +51,11 @@
           </form>
         </div>
         <div class="col-sm-8">
-          <div id="myCarousel" class="carousel slide" data-ride="carousel">
-            <!-- Indicators -->
-            <ol class="carousel-indicators">
-              <?php //  for ($i = 0; $i < sizeof($reviews); $i++) {
-                //if ($i == 0) {
-                  //echo '<li data-target="#myCarousel" data-slide-to="0" class="active"></li>';
-                //} else echo '<li data-target="#myCarousel" data-slide-to="'.$i.'"></li>';
-              //} ?>
-                
-              <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-              <li data-target="#myCarousel" data-slide-to="1"></li>
-            </ol>
-
-            <!-- Wrapper for slides -->
-            <div class="carousel-inner" role="listbox">
+            <div>
               <?php
-                $i = 0;
                 if (sizeof($reviews) > 0) {
                   foreach ($reviews as $review) {
-                    if ($i == 0) {
-                      echo '<div class="item active">';
-                      $i++;
-                    } else echo '<div class="item">';
+                    echo '<div class="item">';
                     echo '<h2>'.$review['title'].'</h2>
                           <div class="col-md-3">
                             <span class="sr-only">'.$review['rating'].' out of Five Stars</span>';
@@ -90,26 +72,13 @@
                           <div>'.$review['timestamp'].'</div>';
                   } 
                 } else die("No reviews for this item.");?>
-
-            <!-- end wrapper for slides -->
             </div>
-
-            <!-- Left and right controls -->
-            <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-              <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-              <span class="sr-only">Previous</span>
-            </a>
-            <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-              <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-              <span class="sr-only">Next</span>
-            </a>
           </div>
         </div>
       </div>
     </div>
   </div>
-</div>
-<hr>
+  <hr>
       
 
 
