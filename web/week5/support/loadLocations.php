b635e1e045572836fd03bdff172a7d11fe73a310
CebrinB <cebrin@byui.edu>
2021-02-19 21:17:53 -0600

    week07

--- web/week5/support/loadLocations.php
+++ web/week5/support/loadLocations.php
@@ -4,7 +4,10 @@ session_start();
 
 require_once 'database.php';
 
-$sql = 'SELECT * FROM ecommerce.location';
+$sql = 'SELECT location.location_name, location.location_phone, 
+        address.address, address.city, address.state, address.zip 
+        FROM ecommerce.location JOIN ecommerce.address 
+        ON location.location_address = address.address_id';
 $stmt = $db->prepare($sql);
 $stmt->execute();
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
