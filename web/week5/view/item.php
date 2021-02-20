d373977d01d1e53570bf8bf9d312927c83e14fdb
CebrinB <cebrin@byui.edu>
2021-02-19 23:01:01 -0600

    week07

--- web/week5/view/item.php
+++ web/week5/view/item.php
@@ -62,6 +62,7 @@
           </div>
         </div>
       </div>
+      </div>
       <div class="row">
         <div class="col-md-6">
           <div><h4><a href="" id="showform">Write a Review</a></h4></div>
@@ -82,7 +83,7 @@
               <input type="submit" value="Save Review">
             </form>
           </div>
-        </div>
+        
       </div>
     </div>
   </div>
