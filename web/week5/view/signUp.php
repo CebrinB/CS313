ee1319eacc396221aab94a966e9c4d210e5b0b6b
CebrinB <cebrin@byui.edu>
2021-02-20 15:32:15 -0600

    week07

--- web/week5/view/signUp.php
+++ web/week5/view/signUp.php
@@ -11,17 +11,17 @@
         } ?>
         <form action="login.php" method="POST">
           <label for="username" class="sr-only">Username:</label><br>
-          <input name="username" id="username" type="text" placeholder="Username" required autofocus>
+          <input class="form-control" name="username" id="username" type="text" placeholder="Username" required autofocus>
           <label for="password" class="sr-only">Password:</label><br>
           <?php if (isset($mark)) {
             echo '<span class="notice">*</span>';
           } ?>
-          <input name="password" id="password" type="password" pattern="^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{7,}$" required>
+          <input class="form-control" name="password" id="password" type="password" pattern="^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{7,}$" required>
           <label for="checkpassword" class="sr-only">Confirm password:</label><br>
           <?php if (isset($mark)) {
             echo '<span class="notice">*</span>';
           } ?>
-          <input name="password2" id="checkpassword" type="password" pattern="^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{7,}$" required>
+          <input class="form-control" name="password2" id="checkpassword" type="password" pattern="^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{7,}$" required>
           <button type="submit">Sign up</button>
           <input type="hidden" name="action" value="signUp">
         </form>
