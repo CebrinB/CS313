74da18ad203db3505f724d178ed3b42a91820df7
CebrinB <cebrin@byui.edu>
2021-02-20 11:33:41 -0600

    week07

--- web/week5/index.php
+++ web/week5/index.php
@@ -1,6 +1,7 @@
 <?php 
   //This is the main controller for this site
   session_start();
+  
   //get the model
   require_once 'model.php';
   
@@ -10,15 +11,45 @@
   }
 
   switch ($action) {
-    case 'register':
-      
-    case 'template':
-      include 'view/template.php';
-      break;
-    default;
-      include '/week5/view/home.php';
-
-  }
-
-
+    case 'signUp':
+       $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
+       $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
+       $password2 = filter_input(INPUT_POST, 'password2', FILTER_SANITIZE_STRING);
+       $pattern = '/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{7,}$/';
+       if (!preg_match($pattern, $password)) {
+           $_SESSION['message'] = "Password must contain 7 characters and number";
+           include "signUp.php";
+           exit;
+       }
+       if ($password != $password2){
+           $_SESSION['message'] = "<p class='notice'>Passwords don't match</p>";
+           $mark = true;
+           include "signUp.php";
+           exit;
+       }
+       $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+       $signUp = signUpUser($db, $username, $hashedPassword);
+       header('Location: signIn.php');
+exit;
+       break;
+   case 'signIn':
+       $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
+       $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
+       $userData = getUser($db, $username);
+       if (password_verify($password, $userData['password'])) {
+           echo 'Password is valid!';
+           $_SESSION['username'] = $username;
+           header('Location: welcome.php');
+           exit;
+       } else {
+           echo 'Invalid password.';
+           include 'signIn.php';
+           exit;
+       }
+       
+       break;
+   default:
+   include "signIn.php";
+   break;
+}
 ?>
--- web/week5/support/scripturesDetails.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'database.php';
-
-$rows = $db -> query('SELECT * FROM ecommerce.item');
-foreach ($rows as $row){
-    echo "<div><b>" . $row['item_id'] . "</b></div>";
-}
-
-?>
