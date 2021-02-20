6a882d428962bba7be5386c3244b007bef494221
CebrinB <cebrin@byui.edu>
2021-02-20 14:43:41 -0600

    week07

--- web/week5/view/signIn.php
+++ web/week5/view/signIn.php
@@ -1,26 +1,28 @@
 <?php include 'head.php' ?>
 <body class="text-center">
     <?php include 'navbar.php'; ?>
-    <div class="container">
-        <div class="col-lg-3"></div>
-            <div class="col-lg-6">
-                <h1>Sign In</h1>
-                <?php
-                if (isset($_SESSION['message'])) {
-                        echo $_SESSION['message'];
-                    }
-                ?>
-                <form method="post" action="login.php" class="form-signin">
-                    <label>Username:</label><br>
-                    <input class="form-control" name="username" type=text required autofocus><br>
-                    <label>Password:</label><br>
-                    <input class="form-control" name="password" type="password" required><br>
-                    <button class="btn btn-lg btn-block" type="submit">Sign In</button>
-                    <input type="hidden" name="action" value="signIn">
-                </form></br>
-                <a href="signUp.php" id="homeshop">Don't have an account? Sign up Today!</a>
-            </div>
-        <div class="col-lg-3"></div>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-3"></div>
+                <div class="col-lg-6">
+                    <h1>Sign In</h1>
+                    <?php
+                    if (isset($_SESSION['message'])) {
+                            echo $_SESSION['message'];
+                        }
+                    ?>
+                    <form method="post" action="login.php" class="form-signin">
+                        <label>Username:</label><br>
+                        <input class="form-control" name="username" type=text required autofocus><br>
+                        <label>Password:</label><br>
+                        <input class="form-control" name="password" type="password" required><br>
+                        <button class="btn btn-lg btn-block" type="submit">Sign In</button>
+                        <input type="hidden" name="action" value="signIn">
+                    </form></br>
+                    <a href="signUp.php" id="homeshop">Don't have an account? Sign up Today!</a>
+                </div>
+            <div class="col-lg-3"></div>
+        </div>
     </div>
     <?php include 'footer.php'; ?>
 </body>
