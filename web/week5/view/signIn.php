3c99114c95bede41a77e1612ae60d43cff3850d7
CebrinB <cebrin@byui.edu>
2021-02-20 15:49:24 -0600

    week07

--- web/week5/view/signIn.php
+++ web/week5/view/signIn.php
@@ -5,7 +5,7 @@
         <div class="row" id="padrow">
             <div class="col-lg-4"></div>
                 <div class="col-lg-4 well">
-                    <h1>please sign in</h1>
+                    <h1>Please sign in</h1>
                     <?php
                     if (isset($_SESSION['message'])) {
                             echo $_SESSION['message'];
