955bcb31e0ecd4ac7558046afdcfdc44a285d353
CebrinB <cebrin@byui.edu>
2021-02-20 22:05:28 -0600

    week07

--- web/week5/support/loadCart.php
+++ web/week5/support/loadCart.php
@@ -5,24 +5,20 @@ session_start();
 require_once 'database.php';
 
 $username = filter_input(INPUT_GET, 'username', FILTER_SANITIZE_STRING);
-echo $username;
 
 $sql = 'SELECT * FROM ecommerce.user WHERE user_name = \''.$username.'\'';
 $stmt = $db->prepare($sql);
 $stmt->execute();
 $user = $stmt->fetch(PDO::FETCH_ASSOC);
-$json = json_encode($user);
- //echo $json;
 
-
-// $sql = "SELECT * FROM ecommerce.cart 
-//         FULL JOIN ecommerce.item on cart.item_id = item.item_id 
-//         WHERE cart.user_id = '".$user['user_id']."'";
-// $stmt = $db->prepare($sql);
-// $stmt->execute();
-// $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-// $json = json_encode($results);
-// echo $json;
+$sql = "SELECT * FROM ecommerce.cart 
+        FULL JOIN ecommerce.item on cart.item_id = item.item_id 
+        WHERE cart.user_id = '".$user['user_id']."'";
+$stmt = $db->prepare($sql);
+$stmt->execute();
+$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$json = json_encode($results);
+echo $json;
 
 $stmt->closeCursor();
 
