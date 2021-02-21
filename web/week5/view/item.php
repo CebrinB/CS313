675c46c64f78932701cd177952f0e753f2623b23
CebrinB <cebrin@byui.edu>
2021-02-20 18:15:26 -0600

    week07

--- web/week5/view/item.php
+++ web/week5/view/item.php
@@ -33,40 +33,38 @@
         <div class="col-md-4 text-center" id="fillpic"><img src="../images/<?php echo $row['item_type'] . $row['item_id'] ?>.jpg"></div>
         <div class="col-md-1"></div>
         <div class="col-md-7">
-        <div class="row">
-          <?php echo '<div><h3>'.$row['item_name'].'</h3></div>';
-                echo '<h4 class="font-weight-bold">$'.$row['item_price'].'</h4>';
-                echo '<div>'.$row['item_description'].'</div>'; ?>
-        </div>
-        <div class="row"><br>
-        <div class="col-md-6"></div>
-        <div class="col-md-6">
-          <div><h3>Reviews</h3></div>
-            <div>
-              <?php
-                if (sizeof($reviews) > 0) {
-                  foreach ($reviews as $review) {
-                    echo '<div class="item">';
-                    echo '<h2>'.$review['title'].'</h2>
-                          <div class="col-md-3">
-                            <span class="sr-only">'.$review['rating'].' out of Five Stars</span>';
-                            $stars = $review['rating'];
-                            for ($i = 0; $i < $stars; $i++) {
-                              echo '<span class="glyphicon glyphicon-star" aria-hidden="true"></span>';
-                            }
-                            $unstars = (5 - $stars);
-                            for ($i = 0; $i < $unstars; $i++) {                                
-                              echo '<span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span>';
-                            }
-                    echo '</div>                          
-                          <div>'.$review['content'].'</div>
-                          <div>'.$review['timestamp'].'</div>';
-                  } 
-                } else echo "No reviews for this item.";?>
+          <div class="row">
+            <div class="col-md-7">
+              <?php echo '<div><h3>'.$row['item_name'].'</h3></div>';
+                    echo '<h4 class="font-weight-bold">$'.$row['item_price'].'</h4>';
+                    echo '<div>'.$row['item_description'].'</div>'; ?>
+            </div>
+          </div>        
+          <div class="row"><br>
+            <div class="col-md-7">
+              <div><h3>Reviews</h3></div>
+                <?php
+                  if (sizeof($reviews) > 0) {
+                    foreach ($reviews as $review) {
+                      echo '<div class="item">';
+                      echo '<h2>'.$review['title'].'</h2>
+                              <div class="col-md-3">
+                                <span class="sr-only">'.$review['rating'].' out of Five Stars</span>';
+                                $stars = $review['rating'];
+                                for ($i = 0; $i < $stars; $i++) {
+                                  echo '<span class="glyphicon glyphicon-star" aria-hidden="true"></span>';
+                                }
+                                $unstars = (5 - $stars);
+                                for ($i = 0; $i < $unstars; $i++) {                                
+                                  echo '<span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span>';
+                                }
+                        echo '</div>                          
+                              <div>'.$review['content'].'</div></div>';
+                    } 
+                  } else echo "No reviews for this item. </div>"; ?>            
             </div>
           </div>
         </div>
-      </div>
       </div>
       
       <div class="row" id="padrow">
