d05a9a48dba345bd2a8d0f64d6fa42dbe98dbb96
CebrinB <cebrin@byui.edu>
2021-02-20 21:39:58 -0600

    week07

--- web/week5/support/loadCart.php
+++ web/week5/support/loadCart.php
@@ -10,15 +10,16 @@ $sql = 'SELECT * FROM ecommerce.user WHERE user_name = \''.$username.'\'';
 $stmt = $db->prepare($sql);
 $stmt->execute();
 $user = $stmt->fetch(PDO::FETCH_ASSOC);
+echo $user['user_id'];
 
-$sql = "SELECT * FROM ecommerce.cart 
-        FULL JOIN ecommerce.item on cart.item_id = item.item_id 
-        WHERE cart.user_id = '".$user['user_id']."'";
-$stmt = $db->prepare($sql);
-$stmt->execute();
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-$json = json_encode($results);
-echo $json;
+// $sql = "SELECT * FROM ecommerce.cart 
+//         FULL JOIN ecommerce.item on cart.item_id = item.item_id 
+//         WHERE cart.user_id = '".$user['user_id']."'";
+// $stmt = $db->prepare($sql);
+// $stmt->execute();
+// $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+// $json = json_encode($results);
+// echo $json;
 
 $stmt->closeCursor();
 
